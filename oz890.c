5c0ae33d03003d56cad34918e0acdaf46805840d
Sergey Alirzaev <zl29ah@gmail.com>
2016-07-31 15:33:58 +0300

    + eeprom reading

--- oz890.c
+++ oz890.c
@@ -1,7 +1,11 @@
+#define _POSIX_C_SOURCE 200809L
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpsse.h>
 #include <assert.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <string.h>
 
 struct mpsse_context *ftdi = NULL;
 uint8_t address = 0x60;
@@ -66,10 +70,56 @@ unsigned read_current(void)
 	return ((unsigned)hi << 8) + lo;
 }
 
+bool is_eeprom_busy(void)
+{
+	uint8_t byte = read_register(0x5f); // EEPROM Control Register
+	return (byte & (1 << 7)); // bit 7 = busy flag
+}
 
-int main(void)
+void read_eeprom_word(uint8_t address, uint8_t *buf)
+{
+	while (is_eeprom_busy()); // wait for eeprom not be busy
+	write_register(0x5e, address); // set eeprom address to read
+
+	while (is_eeprom_busy());
+	write_register(0x5f, 0x55); // b01010101 (or 0x55) set eeprom access & word reading mode
+
+	while (is_eeprom_busy());
+	buf[1] = read_register(0x5d); // odd addr
+	while (is_eeprom_busy());
+	buf[0] = read_register(0x5c); // even addr
+
+	while (is_eeprom_busy());
+	write_register(0x5f, 0x00); // disable eeprom access
+}
+
+const unsigned eeprom_size = 128;
+uint8_t *read_eeprom(void)
+{
+	uint8_t *rv = malloc(eeprom_size);
+	for (unsigned i = 0; i < 128; i += 2) {
+		read_eeprom_word(i, rv + i);
+	}
+	return rv;
+}
+
+
+int main(int argc, char *argv[])
 {
 	int retval = EXIT_FAILURE;
+
+	int opt;
+	char *eeprom_out = NULL;
+	while ((opt = getopt(argc, argv, "o:")) != -1) {
+		switch (opt) {
+		case 'o':
+			eeprom_out = strdup(optarg);
+			break;
+		default:
+			fprintf(stderr, "Usage: %s [-o filename]\n", argv[0]);
+			return retval;
+		}
+	}
 
 	if((ftdi = MPSSE(I2C, FOUR_HUNDRED_KHZ, MSB)) != NULL && ftdi->open)
 	{
@@ -97,6 +147,13 @@ int main(void)
 				read_cell_voltage(cell);
 			}
 			printf("Current: %u", read_current());
+			if (eeprom_out) {
+				FILE *f = fopen(eeprom_out, "wb");
+				uint8_t *buf = read_eeprom();
+				fwrite(buf, eeprom_size, 1, f);
+				fclose(f);
+				free(buf);
+			}
 		}
 	} else {
 		fprintf(stderr, "Failed to initialize MPSSE: %s\n", ErrorString(ftdi));
