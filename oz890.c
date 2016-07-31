f3efaf13928ab8bbb31c61553fb5a01aba3b23ac
Sergey Alirzaev <zl29ah@gmail.com>
2016-07-31 04:43:27 +0300

    sketchy voltage reading

--- oz890.c
+++ oz890.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpsse.h>
+#include <assert.h>
 
 struct mpsse_context *ftdi = NULL;
-char address = 0x60;
+uint8_t address = 0x60;
 
-char read_register(char reg)
+uint8_t read_register(uint8_t reg)
 {
-	char *data = NULL;
-	char rv;
-	char addr_reading = address | 1;
+	uint8_t *data = NULL;
+	uint8_t rv;
+	uint8_t addr_reading = address | 1;
 
 	Start(ftdi);
-	Write(ftdi, &address, 1);
-	Write(ftdi, &reg, 1);
+	Write(ftdi, (char *)&address, 1);
+	Write(ftdi, (char *)&reg, 1);
 
 	if(GetAck(ftdi) == ACK) {
 		Start(ftdi);
-		Write(ftdi, &addr_reading, 1);
+		Write(ftdi, (char *)&addr_reading, 1);
 
 		if(GetAck(ftdi) == ACK) {
-			data = Read(ftdi, 1);
+			data = (uint8_t *)Read(ftdi, 1);
 			if(data) {
 				printf("got %x\n", *(data));
 				rv = *data;
@@ -37,15 +38,27 @@ char read_register(char reg)
 	return rv;
 }
 
-void write_register(char reg, char data)
+void write_register(uint8_t reg, uint8_t data)
 {
 	Start(ftdi);
-	Write(ftdi, &address, 1);
-	Write(ftdi, &reg, 1);
-	Write(ftdi, &data, 1);
+	Write(ftdi, (char *)&address, 1);
+	Write(ftdi, (char *)&reg, 1);
+	Write(ftdi, (char *)&data, 1);
 
 	Stop(ftdi);
 }
+
+
+unsigned read_cell_voltage(unsigned cell)
+{
+	assert(cell < 13);
+	uint8_t lo = read_register(0x32 + cell * 2);
+	uint8_t hi = read_register(0x33 + cell * 2);
+	unsigned rv = ((unsigned)hi << 5) + (lo >> 3);
+	printf("cell %d: %d %d %lfmV\n", cell, hi, lo, 1.22 * rv);
+	return rv;
+}
+
 
 int main(void)
 {
@@ -54,14 +67,14 @@ int main(void)
 	if((ftdi = MPSSE(I2C, FOUR_HUNDRED_KHZ, MSB)) != NULL && ftdi->open)
 	{
 		printf("%s initialized at %dHz (I2C)\n", GetDescription(ftdi), GetClock(ftdi));
-		char chip_id = read_register(0);
+		uint8_t chip_id = read_register(0);
 		if (chip_id != 2) {
 			fprintf(stderr, "Unknown chip: %x\n", chip_id);
 			goto out;
 		} else {
 			printf("OZ890 rev C detected.\n");
-			char softsleep = read_register(0x14);
-			char shutdown = read_register(0x15);
+			uint8_t softsleep = read_register(0x14);
+			uint8_t shutdown = read_register(0x15);
 			if (shutdown & 0x10) {
 				printf("Battery is unbalanced (permanent failure flag). Clearing...\n");
 				write_register(0x15, 0x10);
@@ -72,7 +85,10 @@ int main(void)
 				printf("Voltage High Permanent Failure.\n");
 			if (shutdown & 0x2)
 				printf("Voltage Low Permanent Failure.\n");
-			char check_yes = read_register(0x1c);
+			uint8_t check_yes = read_register(0x1c);
+			for (int cell = 0; cell < 13; ++cell) {
+				read_cell_voltage(cell);
+			}
 		}
 	} else {
 		fprintf(stderr, "Failed to initialize MPSSE: %s\n", ErrorString(ftdi));
